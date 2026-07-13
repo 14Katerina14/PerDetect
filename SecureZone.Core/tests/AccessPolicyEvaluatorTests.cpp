cb01f05c7be38a80dde1022ae0921c0bceac0662
AngelinaSvetlozarova <127393773+AngelinaSvetlozarova@users.noreply.github.com>
2026-07-13 10:18:24 +0300

    Add tests for access policy evaluator

---
+++ SecureZone.Core/tests/AccessPolicyEvaluatorTests.cpp
@@ -0,0 +1,12 @@
+#include <cassert>
+#include "securezone/decision/AccessPolicyEvaluator.h"
+int main() {
+    using namespace securezone::domain; using securezone::decision::AccessPolicyEvaluator;
+    const AccessPolicy policy{"p", "z", {"operator"}, {MachineStatus::Stopped}}; const MachineState stopped{"m", MachineStatus::Stopped};
+    const Employee allowed{"e", "Name", "Operator", {"operator"}}; const Employee denied{"e", "Name", "Guest", {"guest"}};
+    AccessPolicyEvaluator evaluator;
+    assert(evaluator.evaluate(allowed, stopped, policy).type == AccessDecisionType::Allowed);
+    assert(evaluator.evaluate(denied, stopped, policy).type == AccessDecisionType::Violation);
+    assert(evaluator.evaluate(allowed, {"m", MachineStatus::Running}, policy).type == AccessDecisionType::Violation);
+    assert(evaluator.evaluate(std::nullopt, stopped, policy).type == AccessDecisionType::UnknownIdentity);
+}
